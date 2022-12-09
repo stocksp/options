de570c98e8924937c627b8c8d3ef6c500a8412bd
Paul Stocks <paul.stocks@gmail.com>
2022-12-08 18:29:59 -0800

    getOptions update
    call with 3 params to get accurate strikes.
    optionsType is either 'calls' or 'puts'
    http://localhost:3001/api/getOptions?name=AAPL&optionDate=2023-07-21&optionsType=calls
    see api.test.http for example

--- pages/api/getOptions.js
+++ pages/api/getOptions.js
@@ -2,7 +2,8 @@ import yahooFinance from "yahoo-finance2";
 const handler = async (req, res) => {
     const name = req.query.name;
     const date = req.query.optionDate;
-    console.log("starting getOptions:", name, date);
+    const optionsType = req.query.optionsType;
+    console.log("starting getOptions:", name, date, optionsType);
     try {
         const queryOptions = {
             lang: "en-US",
@@ -11,7 +12,22 @@ const handler = async (req, res) => {
             date,
         };
         const result = await yahooFinance.options(name, queryOptions);
-        res.json(result);
+        const callOrPuts = result.options[0][optionsType];
+        const theData = callOrPuts.map(d => {
+            return {
+                "contractSymbol": d.contractSymbol,
+                "strike": d.strike,
+                "lastPrice": d.lastPrice,
+            }
+        });
+        const data = {
+            name: req.query.name,
+            date: req.query.optionDate,
+            optionsType: req.query.optionsType,
+            data: theData,
+
+        };
+        res.json(data);
     } catch (error) {
         console.log("error thrown in getOptions", error);
         res.json("Error: " + error.toString());
