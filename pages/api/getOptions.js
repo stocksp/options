da90ff82e839c1644eb9ee575b34fc43c15547dd
Paul Stocks <paul.stocks@gmail.com>
2022-12-12 13:27:37 -0800

    Added Redis cache for getOptions
    need my .env.local for upstash

--- pages/api/getOptions.js
+++ pages/api/getOptions.js
@@ -1,10 +1,19 @@
 import yahooFinance from "yahoo-finance2";
+import redis from "../../lib/redis";
+
 const handler = async (req, res) => {
     const name = req.query.name;
     const date = req.query.optionDate;
     const optionsType = req.query.optionsType;
     console.log("starting getOptions:", name, date, optionsType);
     try {
+        const key = `${name}:${date}:${optionsType}`;
+        const cache = await redis.get(key);
+        if(cache) {
+            console.log(`key ${key} from cache`);
+            res.json(cache);
+            return;
+        }
         const queryOptions = {
             lang: "en-US",
             formatted: false,
@@ -27,6 +36,8 @@ const handler = async (req, res) => {
             data: theData,
 
         };
+        console.log(`sending ${key} to cache`)
+        await redis.setex(key, 1800, JSON.stringify(data));
         res.json(data);
     } catch (error) {
         console.log("error thrown in getOptions", error);
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,33 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+// import { Redis } from "@upstash/redis";
+
+// const redis = new Redis({
+//   url: "https://usw1-stirring-sheepdog-33238.upstash.io",
+//   token:
+//     "AYHWASQgMjI2ZTMxNTYtN2U5Yi00ZTI1LThmZjMtODI0MTFmMzI3NDY3YjQ5N2YyNjU1NmU1NGI5ZDkyYmY3MzY0ZTA5NzI3MGQ=",
+// });
+import redis from "../../lib/redis";
+
+export default async function handler(req, res) {
+  const count = await redis.incr("counter");
+  const someData = {
+    name: "Roberta McDonald",
+    pets: [
+      {
+        name: "Rex",
+        species: "dog",
+        age: 3,
+        isMammal: true,
+      },
+      {
+        name: "Goldie",
+        species: "fish",
+        age: 2,
+        isMammal: false,
+      },
+    ],
+  };
+  await redis.set("paulTest", JSON.stringify(someData));
+  res.status(200).json({ count, someData });
 }
