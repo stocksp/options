3545c00d53e96f8332087b98fcc7a3fc018ba23a
Ben Stocks <ben@solenlabs.com>
2022-12-14 10:40:26 -0800

    Add back cache controls

--- pages/api/contractsByExpiration.ts
+++ pages/api/contractsByExpiration.ts
@@ -1,55 +1,71 @@
-import yahooFinance from "yahoo-finance2";
-import redis from "../../lib/redis";
+import yahooFinance from "yahoo-finance2"
+import redis from "../../lib/redis"
 
 const handler = async (
-  req: { query: { ticker: string; expirationDate: string } },
+  req: {
+    query: {
+      ticker: string
+      expirationDate: string
+      bustCache: string
+      cacheTTL: number
+    }
+  },
   res: any
 ) => {
-  const ticker = req.query.ticker;
-  const expirationDate = req.query.expirationDate;
-  console.log("starting contractsByExpiration:", ticker, expirationDate);
+  const ticker = req.query.ticker
+  const expirationDate = req.query.expirationDate
+  const bustCache = req.query.bustCache
+  const cacheTTL = req.query.cacheTTL
+  console.log("starting contractsByExpiration:", ticker, expirationDate)
   try {
-    const key = `${ticker}:${expirationDate}`;
-    const cache = await redis.get(key);
-    if (cache) {
-      console.log(`key ${key} from cache`);
-      res.json(cache);
-      return;
+    const key = `${ticker}:${expirationDate}`
+    if (bustCache == "true") {
+      console.log("Busting redis cache")
+    } else {
+      const cache = await redis.get(key)
+      if (cache) {
+        console.log(`key ${key} from cache`)
+        res.json(cache)
+        return
+      }
     }
     const queryOptions = {
       lang: "en-US",
       formatted: false,
       region: "US",
       date: expirationDate,
-    };
-    const result = await yahooFinance.options(ticker, queryOptions);
-    const callsObjArr = result.options[0]["calls"].map((d) => {
+    }
+    const result = await yahooFinance.options(ticker, queryOptions)
+    const optionsData = result.options[0]
+    console.log(result.options)
+    const callsObjArr = optionsData.calls.map((d) => {
       return {
         contractSymbol: d.contractSymbol,
         strike: d.strike,
         lastPrice: d.lastPrice,
-      };
-    });
-    const putsObjArr = result.options[0]["puts"].map((d) => {
+      }
+    })
+    const putsObjArr = optionsData.puts.map((d) => {
       return {
         contractSymbol: d.contractSymbol,
         strike: d.strike,
         lastPrice: d.lastPrice,
-      };
-    });
+      }
+    })
     const returnData = {
       ticker: ticker,
       date: expirationDate,
       calls: callsObjArr,
       puts: putsObjArr,
-    };
-    console.log(`sending ${key} to cache`);
-    await redis.setex(key, 1800, JSON.stringify(returnData));
-    res.json(returnData);
+    }
+    const ttl = cacheTTL ? cacheTTL : 1800
+    console.log(`sending ${key} to cache with ttl ${ttl}`)
+    await redis.setex(key, ttl, JSON.stringify(returnData))
+    res.json(returnData)
   } catch (error: any) {
-    console.log("error thrown in getOptions", error);
-    res.json("Error: " + error.toString());
+    console.log("error thrown in contractsByExpiration", error)
+    res.json("Error: " + error.toString())
   }
-};
+}
 
-export default handler;
+export default handler
